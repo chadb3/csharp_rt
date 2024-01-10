6f91bfda6b050b4ed0b4135560bc37fc26599ae2
chad <chadb78999@gmail.com>
2024-01-10 00:27:25 -0600

    Worked on ClockFace  - this is a continuation of what I had in "CSharpRayTracer.cs".  - I moved it as it might get deleted in future "clean ups" if lest in main.

--- ClockFace/ClockFace.cs
+++ ClockFace/ClockFace.cs
@@ -13,20 +13,46 @@ class ClockFace
     {
         Console.WriteLine("Hello, World!");
         csharp_rt.Tuple center = csharp_rt.Tuple.point(0, 0, 0);
-        Canvas c = new Canvas(100, 100);
+        csharp_rt.Tuple twelve = csharp_rt.Tuple.point(0, 0, 1);
+        int height = 100;
+        int width = 100;
+        Canvas c = new Canvas(height, width);
+        // we will add the width back in the move function...
+        double midPoint = height / 2;
+        //writing 12oclock to the canvas
+        csharp_rt.Tuple CurrentPoint = twelve;
+        //twelve = compute(twelve);
+        //twelve = move(twelve,height,width);
+        //pixle at 0 0 was a test print as I had a hard time finding the ppm file...
+        //however i found that it is writing the center pixle correctly!
+        //c.write_pixel(0, 0, Color.RED());
+        c.write_pixel(50, 50, Color.Green());
+        //c.write_pixel((int)twelve.x, (int)twelve.z, Color.RED());
+        for(int i = 0;i<12;i++)
+        {
+            Console.WriteLine("i:{0}",i);
+            twelve = compute(twelve);
+            //twelve = twelve+midPoint;
+            Console.WriteLine(twelve);
+            twelve = move(twelve, height, width);
+            
+            //c.write_pixel((int)twelve.x, (int)twelve.z, Color.RED());
+        }
+        c.canvas_to_P3_ppm();
         return 0;
     }
-
+    // this function returns the new pixle location after being rotated.
     public static csharp_rt.Tuple compute(csharp_rt.Tuple tupleIn)
     {
         csharp_rt.Tuple retVal=tupleIn;
         Matrix r = Matrix.rotation_y(3 * Math.PI / 6);
-        return retVal;
+        return r*retVal;
     }
 
-    public static csharp_rt.Tuple move(csharp_rt.Tuple tupleIn)
-    { 
-        return tupleIn; 
+    // this function moves the pixles to the middle of the canvas
+    public static csharp_rt.Tuple move(csharp_rt.Tuple tupleIn, int heightIn, int widthIn)
+    {
+        return tupleIn + csharp_rt.Tuple.point(heightIn/2, 0, widthIn/2) ; 
     }
 
 }
