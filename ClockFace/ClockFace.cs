69732c7d8cf4d18bd556b9056300ef41145a8817
chad <chadb78999@gmail.com>
2024-01-10 06:26:27 -0600

    Worked on ClockFace  - added a method to test from the y axis.

--- ClockFace/ClockFace.cs
+++ ClockFace/ClockFace.cs
@@ -7,6 +7,7 @@ using csharp_rt;
 // and then move them to the center of the canvas by adding the cords of the center point.
 // let x be x cords of the pixel and z be the y coordinate. 
 /// <summary>
+/// // most of this exercise was trying to remember how canvas worked...
 /// 1. Establish a center point (k)
 /// 2. Create the twelve O'clock tuple. (book says (0,0,1))
 /// 3. establish the rotation matrix as rotation_y(pi/6) each hour will be multiplied by this.
@@ -22,7 +23,7 @@ using csharp_rt;
 ///     13. then write the pixle to the canvas
 ///     14. loop to 12 compleating steps 9-13 for each.
 /// 15. write canvas to ppm.
-/// Note: Book had us align it along the z axis and the 12,3,6,9 appear to be angled.
+/// Note: Book had us align it along the z axis and the 12,3,6,9 appear to be angled. I re reviewed my old code, and this might be wrong as I didn't change colors for the 12,3,6,9 I just had it all green.
 /// looking at the book this appears to be intended.
 /// I did this example in the past along the y axis and it appeared to work as expected (12 o'clock at the top)
 /// to do this you would use rotation_z(pi/6) and use the y for y (instead z for y).
@@ -78,8 +79,59 @@ class ClockFace
         //end test
 
         C.canvas_to_P3_ppm();
-       
+        testYAxis();
         return 0;
+    }
+    /// <summary>
+    /// This is an experiment to try to get it so that 12,3,6,9 are green.
+    /// </summary>
+    static void testYAxis()
+    {
+        int height = 100, width = 100;
+        Canvas C = new Canvas(width + 100, height + 100);
+        C.set_file_name("y_axis.ppm");
+        csharp_rt.Tuple k = csharp_rt.Tuple.point(0, 0, 0);
+        csharp_rt.Tuple twelve = csharp_rt.Tuple.point(0, 1, 0);
+        Matrix rotation = Matrix.rotation_z(Math.PI / 6d);
+        csharp_rt.Tuple add = csharp_rt.Tuple.vector(100, 100,0);
+        double radius = 0.9d * height;
+        k = k * radius;
+        k = k + add;
+        //Console.WriteLine(k);
+        C.write_pixel(k.x, k.y, csharp_rt.Color.Green());
+        //twelve= twelve * radius;
+        //twelve = twelve + csharp_rt.Tuple.vector(100, 100, 0);
+        //this spawns the pixle at the bottom (6 o'clock). If I remember, everything on canvas is filpped. 
+       // C.write_pixel(twelve.x, twelve.y, new Color(.34, .56,.82));
+        for (int i = 0; i < 12; i++)
+        {
+            //Console.WriteLine(twelve);
+            k = rotation * twelve;
+            //int a = i <= 0 ? 1 : i;
+            //Console.WriteLine("a: {0}",a);
+           // k=Matrix.rotation_z(a*Math.PI/6)*twelve;
+            twelve = k;
+            //Console.WriteLine(twelve);
+            k = k * radius;
+            k = k + add;
+            if (i % 3 == 0&&i>0/*twelve.x == 1 || twelve.y == 1 || twelve.z == 1 || i == 0*/)
+            {
+                Console.WriteLine("{0}", i);
+                //Console.WriteLine(twelve);
+                C.write_pixel(k.x, 200-k.y, Color.Green());
+            }
+            else if(i==0)
+            {
+                C.write_pixel(k.x, 200 - k.y, new Color(164,50,168));
+            }
+            else
+            {
+                Console.WriteLine("else: {0}", i);
+                //Console.WriteLine(twelve);
+                C.write_pixel(k.x, 200-k.y, Color.RED());
+            }
+        }
+        C.canvas_to_P3_ppm();
     }
   
 
