fc9780aa044055dca71dc7baf97f1c91365bdc78
chad <chadb78999@gmail.com>
2024-01-10 03:52:35 -0600

    Worked on ClockFace  - is (technically) working, but doesn't print as I expected   - (my expected 12 o'clock is at the 5 o'clock position)

--- ClockFace/ClockFace.cs
+++ ClockFace/ClockFace.cs
@@ -11,7 +11,48 @@ class ClockFace
 
     public static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello World");
+        int height = 100, width = 100;
+        Canvas C = new Canvas(width+100, height+100);
+        csharp_rt.Tuple add = csharp_rt.Tuple.vector(100, 0, 100);
+        csharp_rt.Tuple k = csharp_rt.Tuple.point(0, 0, 0);
+        csharp_rt.Tuple twelve = csharp_rt.Tuple.point(0, 0, 1);
+        Matrix rotation = Matrix.rotation_y(Math.PI / 6);
+        double radius = 0.5d * height;
+        k = k + add;
+        Console.WriteLine(k);
+        C.write_pixel(k.x, k.z, csharp_rt.Color.Green());
+        for(int i=0;i<12;i++)
+        {
+            k = rotation * twelve;
+            twelve = k;
+            k = k * radius;
+            k = k + add;
+            if (k.x == 1 || k.y == 1 || k.z == 1 || i == 0)
+            {
+                C.write_pixel(k.x, k.z, Color.Green());
+            }
+            else
+            {
+                C.write_pixel(k.x, k.z, Color.RED());
+            }
+        }
+        //test
+        /*k = rotation * twelve;
+        // make 12 be k
+        twelve = k;
+        k = k * radius;
+        k = k + add;
+        C.write_pixel(k.x, k.z, Color.RED());
+        k = rotation * twelve;
+        twelve = k;
+        k = k * radius;
+        k = k + add;
+        C.write_pixel(k.x, k.z, Color.RED());*/
+        //end test
+
+        C.canvas_to_P3_ppm();
+        /*Console.WriteLine("Hello, World!");
         csharp_rt.Tuple center = csharp_rt.Tuple.point(0, 0, 0);
         csharp_rt.Tuple twelve = csharp_rt.Tuple.point(0, 0, 1);
         int height = 100;
@@ -21,6 +62,8 @@ class ClockFace
         double midPoint = height / 2;
         //writing 12oclock to the canvas
         csharp_rt.Tuple CurrentPoint = twelve;
+
+        double radius = 1 / 2 * height;
         //twelve = compute(twelve);
         //twelve = move(twelve,height,width);
         //pixle at 0 0 was a test print as I had a hard time finding the ppm file...
@@ -30,15 +73,18 @@ class ClockFace
         //c.write_pixel((int)twelve.x, (int)twelve.z, Color.RED());
         for(int i = 0;i<12;i++)
         {
-            Console.WriteLine("i:{0}",i);
-            twelve = compute(twelve);
+            //Console.WriteLine("i:{0}",i);
+            CurrentPoint = compute(twelve);
+            twelve = CurrentPoint;
+            Console.WriteLine("Current point before radius: {0}",CurrentPoint);
+            CurrentPoint = CurrentPoint * radius;
             //twelve = twelve+midPoint;
-            Console.WriteLine(twelve);
-            twelve = move(twelve, height, width);
-            
-            //c.write_pixel((int)twelve.x, (int)twelve.z, Color.RED());
+            //Console.WriteLine(twelve);
+            CurrentPoint = move(CurrentPoint, height, width);
+            Console.WriteLine(CurrentPoint);
+            c.write_pixel(Math.Abs((int)CurrentPoint.x), Math.Abs((int)CurrentPoint.z), Color.RED());
         }
-        c.canvas_to_P3_ppm();
+        c.canvas_to_P3_ppm();*/
         return 0;
     }
     // this function returns the new pixle location after being rotated.
