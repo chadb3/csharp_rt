a63783519114c62b6854d87db3345bdda95a0ed5
chad <chadb78999@gmail.com>
2024-01-13 07:40:56 -0600

    Worked on ClockFace  - renamed Z_ClockFace to appear lower on the Solution Explorer Started working on Ray Started to renamed class files to start with a capital letter.

---
+++ csharp_rt/Ray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace csharp_rt
+{
+    public class Ray
+    {
+         public Ray(Tuple origin, Tuple vec) 
+        { 
+
+        }
+    }
+}
