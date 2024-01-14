996efad452888ad705f4f40f27d788f86226329f
chad <chadb78999@gmail.com>
2024-01-14 08:05:50 -0600

    Worked on Ray  - added warnings if w is incorrect for origin and vec

--- csharp_rt/Ray.cs
+++ csharp_rt/Ray.cs
@@ -9,9 +9,25 @@ namespace csharp_rt
 {
     public class Ray
     {
+        Tuple origin;
+        Tuple vec;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="origin">Point w=0</param>
+        /// <param name="vec">Vector w=1</param>
          public Ray(Tuple origin, Tuple vec) 
-        { 
-
+        {
+            this.origin = origin;
+            this.vec = vec;
+            if(origin.w!=0)
+            {
+                Console.WriteLine("Warning: Origin Point w != 0");
+            }
+            if(vec.w!=1)
+            {
+                Console.WriteLine("Warning Ray Vector w != 0");
+            }
         }
     }
 }
