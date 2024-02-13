2500837ddcf4309449a4c173aac276792bc7c645
chad <chadb78999@gmail.com>
2024-02-13 04:42:17 -0600

    Started working on z_ray_cast_sphere  Starting on project to make a sphere cast sphere image.

---
+++ z_ray_cast_sphere/sphereImage.cs
@@ -0,0 +1,12 @@
+﻿using csharp_rt;
+namespace z_ray_cast_sphere
+{
+    internal class sphereImage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
