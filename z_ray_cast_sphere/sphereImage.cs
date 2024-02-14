f8ae5a0b75807a8101223bd48e6c59d1c989d899
chad <chadb78999@gmail.com>
2024-02-14 04:30:09 -0600

    Started working on z_ray_cast_sphere  Starting on project to make a sphere cast sphere image.  started coding based off the exercise.

--- z_ray_cast_sphere/sphereImage.cs
+++ z_ray_cast_sphere/sphereImage.cs
@@ -5,8 +5,21 @@ namespace z_ray_cast_sphere
     {
         static void Main(string[] args)
         {
+            Canvas c;
             Console.WriteLine("Hello, World!");
-            
+            // origin of the ray
+            csharp_rt.Tuple ray_origin = csharp_rt.Tuple.point(0, 0, -5);
+            // wall location
+            int wall_z = 10;
+            // according to the math wall size can be 6 min
+            // we set it to 7 here. 
+            double wall_size = 7.0d;
+            //size of the image 100x100
+            int canvas_pixels = 100;
+            // our pixle size based off of the size of the wall and
+            double pixel_size = wall_size / canvas_pixels;
+            //half the wall size. this assumes you are looking at the center of the circle. 
+            double half = wall_size / 2;
         }
     }
 }
