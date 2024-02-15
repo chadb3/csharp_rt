58ca19f22ddaf825a4451ec0a7b18d1672c8b346
chad <chadb78999@gmail.com>
2024-02-15 07:49:17 -0600

    Started working on z_ray_cast_sphere  continued to work on this.

--- z_ray_cast_sphere/sphereImage.cs
+++ z_ray_cast_sphere/sphereImage.cs
@@ -5,7 +5,7 @@ namespace z_ray_cast_sphere
     {
         static void Main(string[] args)
         {
-            Canvas c;
+            
             Console.WriteLine("Hello, World!");
             // origin of the ray
             csharp_rt.Tuple ray_origin = csharp_rt.Tuple.point(0, 0, -5);
@@ -20,6 +20,25 @@ namespace z_ray_cast_sphere
             double pixel_size = wall_size / canvas_pixels;
             //half the wall size. this assumes you are looking at the center of the circle. 
             double half = wall_size / 2;
+            //creating the canvas
+            Canvas canvas = new Canvas(canvas_pixels, canvas_pixels);
+            // adding color red
+            Color color = new Color(1, 0, 0);
+            // adding sphere shape
+            Sphere shape= new Sphere();
+            //ray casting loop.
+            for(int y=0;y< canvas_pixels-1;y++)
+            {
+
+                //y coordinate
+                double world_y = half - pixel_size * y;
+                for(int x=0;x< canvas_pixels - 1; x++)
+                {
+                    //x coord
+                    double world_x=-half+pixel_size * x;
+                    csharp_rt.Tuple position= csharp_rt.Tuple.point(world_x, world_y, wall_z);
+                }
+            }
         }
     }
 }
