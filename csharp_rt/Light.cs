ddd22fdea315427685e24a8b326bab7c3b76199e
chad <chadb78999@gmail.com>
2024-02-29 07:50:48 -0600

    Starting on work on Light Class. didn't notice it wanted this implemented earlier.

---
+++ csharp_rt/Light.cs
@@ -0,0 +1,20 @@
+﻿using csharp_rt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRayTracer
+{
+    public class Light
+    {
+        public csharp_rt.Tuple position;
+        public Color intensity;
+        public Light()
+        {
+            this.position = csharp_rt.Tuple.point(0, 0, 0);
+            this.intensity = new Color(0, 0, 0);
+        }
+    }
+}
