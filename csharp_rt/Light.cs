8f93b204bc0525642a89fc545af966dce3c2fb9e
chad <chadb78999@gmail.com>
2024-03-01 08:08:44 -0600

    Working on light

--- csharp_rt/Light.cs
+++ csharp_rt/Light.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,19 @@ namespace CSharpRayTracer
         {
             this.position = csharp_rt.Tuple.point(0, 0, 0);
             this.intensity = new Color(0, 0, 0);
+        }
+        public Light(csharp_rt.Tuple position, Color intensity)
+        {
+            this.position = position;
+            this.intensity = intensity;
+        }
+
+        public static Light point_light(csharp_rt.Tuple position, Color intensity)
+        {
+            Light pl=new Light();
+            pl.intensity = intensity;
+            pl.position = position;
+            return pl;
         }
     }
 }
