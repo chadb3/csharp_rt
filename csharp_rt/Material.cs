68f35cea18a4b1af8c1ad3c0b0c7d5c6b158ef6a
chad <chadb78999@gmail.com>
2024-03-04 03:58:04 -0600

    Working on Material

--- csharp_rt/Material.cs
+++ csharp_rt/Material.cs
@@ -8,5 +8,18 @@ namespace csharp_rt
 {
     public class Material
     {
+        public Color color;
+        public double ambient;
+        public double diffuse;
+        public double specular;
+        public double shininess;
+        public Material()
+        {
+            color = new Color(1, 1, 1);
+            ambient = 0.1d;
+            diffuse = 0.9d;
+            specular = 0.9d;
+            shininess = 200.0d;
+        }
     }
 }
