cae341a834f1e68370f3cb2dbb76b81d1a382256
chad <chadb78999@gmail.com>
2024-04-11 16:04:02 -0500

    still working on lighting.

--- csharp_rt/Light.cs
+++ csharp_rt/Light.cs
@@ -56,7 +56,12 @@ namespace csharp_rt
                 csharp_rt.Color color_diffuse = effective_color * m.diffuse * light_dot_normal;
                 csharp_rt.Tuple reflectV = -normalv.reflect(eyev);
                 double reflect_dot_eye=reflectV.dot(eyev);
+                if (reflect_dot_eye <= 0)
+                {
+                    
+                }
             }
+            
             return ret;
         }
     }
