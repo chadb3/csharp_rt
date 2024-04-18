8d117f4cdfe0f4ca5d2114504e720fe76f91c450
chad <chadb78999@gmail.com>
2024-04-18 08:06:28 -0500

    Starting putting it together project.

---
+++ Lighting_Putting_It_Together/Program.cs
@@ -0,0 +1,11 @@
+﻿using csharp_rt;
+namespace Z_Lighting_Putting_It_Together
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
