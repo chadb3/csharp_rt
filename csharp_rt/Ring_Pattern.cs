227ab13def8ad3fdf941cd05e32329d1cd3167e7
chad <chadb78999@gmail.com>
2024-11-02 07:27:39 -0500

    git starting on RIng Pattern

---
+++ csharp_rt/Ring_Pattern.cs
@@ -0,0 +1,21 @@
+﻿using csharp_rt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRayTracer
+{
+    public class Ring_Pattern : Pattern
+    {
+        public Ring_Pattern():base()
+        { 
+        
+        }
+        protected override Color Pattern_At(csharp_rt.Tuple point_in)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
