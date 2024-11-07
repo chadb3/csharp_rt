9c2e3f0db934d0aaf033e4d6cd45e2898783efaf
chad <chadb78999@gmail.com>
2024-11-07 06:54:50 -0600

    started work on a Solid_Pattern to assist in nesting patterns

---
+++ csharp_rt/Solid_Pattern.cs
@@ -0,0 +1,30 @@
+﻿using csharp_rt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRayTracer
+{
+    public class Solid_Pattern : Pattern
+    {
+
+        public Color Color { get; set; }
+        public Solid_Pattern() : base() 
+        { 
+            Color=new Color(0,0,0);
+        }
+
+        public Solid_Pattern(Color Color):base()
+        {
+            this.Color = Color;
+        }
+
+        public override Color Pattern_At(csharp_rt.Tuple point_in)
+        {
+            return Color;
+        }
+
+    }
+}
