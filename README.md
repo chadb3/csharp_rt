d34eb4344d4e223071be6de906b206f4bdcc8fa7
chad <chadb78999@gmail.com>
2024-11-11 07:11:41 -0600

    trying to update readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 My (WIP) implementation of a ray tracer using the book The Ray Tracer Challenge by Jamis Buck 
 
+Current Capabilities:
+[/readme_images/cc.png]
--- csharp_rt/Checker_Pattern.cs
+++ csharp_rt/Checker_Pattern.cs
@@ -16,6 +16,7 @@ namespace CSharpRayTracer
         {
             Color_A = new Color(1, 1, 1);
             Color_B = new Color(0, 0, 0);
+            // note below doesn't seem to work, and I still need to set this manually in Material.Pattern.Set_Transform(...below);
             Set_Transform(Matrix.translation(0, 0.000001d, 0));
             //Source: https://forum.raytracerchallenge.com/thread/290/bands-noise
         }
@@ -23,6 +24,7 @@ namespace CSharpRayTracer
         {
             this.Color_A = Color_A;
             this.Color_B = Color_B;
+            // note below doesn't seem to work, and I still need to set this manually in Material.Pattern.Set_Transform(...below);
             Set_Transform(Matrix.translation(0, 0.0001d, 0));
             //Source: https://forum.raytracerchallenge.com/thread/290/bands-noise
         }
