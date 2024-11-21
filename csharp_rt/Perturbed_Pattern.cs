64144637ed413c500aab123a0d8e7a96aa25bb6c
chad <chadb78999@gmail.com>
2024-11-21 07:03:59 -0600

    started new and last pattern (for now).

---
+++ csharp_rt/Perturbed_Pattern.cs
@@ -0,0 +1,30 @@
+﻿using csharp_rt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRayTracer
+{
+    public class Perturbed_Pattern : Pattern
+    {
+        public Pattern Main_Pattern { get; set; }
+        public Perturbed_Pattern():base()
+        {
+            Main_Pattern = new Solid_Pattern(new Color(0.02, 0.03, 0.04));
+        }
+        public Perturbed_Pattern(Color color_in):base()
+        {
+            Main_Pattern=new Solid_Pattern(color_in);
+        }
+        public Perturbed_Pattern(Pattern Pattern_in):base()
+        {
+            Main_Pattern=Pattern_in;
+        }
+        public override Color Pattern_At(csharp_rt.Tuple point_in)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
