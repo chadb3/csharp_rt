fc4b1c13617e27cd574b07528e359b229817f77a
chad <chadb78999@gmail.com>
2024-11-22 07:22:00 -0600

    worked on patterns (stopping for now) will start on Chapter 11 - Reflection and Refractions

--- csharp_rt/Perturbed_Pattern.cs
+++ csharp_rt/Perturbed_Pattern.cs
@@ -10,18 +10,22 @@ namespace CSharpRayTracer
     public class Perturbed_Pattern : Pattern
     {
         public Pattern Main_Pattern { get; set; }
+        // WIP - revisit later
         public Perturbed_Pattern():base()
         {
             Main_Pattern = new Solid_Pattern(new Color(0.02, 0.03, 0.04));
         }
+        // WIP - revisit later
         public Perturbed_Pattern(Color color_in):base()
         {
             Main_Pattern=new Solid_Pattern(color_in);
         }
+        // WIP - revisit later
         public Perturbed_Pattern(Pattern Pattern_in):base()
         {
             Main_Pattern=Pattern_in;
         }
+        // WIP - revisit later
         public override Color Pattern_At(csharp_rt.Tuple point_in)
         {
             throw new NotImplementedException();
